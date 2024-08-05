fba3e03f753d6850229fb0dc7b7e309bc0dc13b7
paolo todini <paolotodini@paolos-MacBook-Air.local>
2024-08-05 17:09:10 +0200

    setup gh-pages

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -6,8 +6,8 @@ export default function NoMatch() {
   return (
     <>
         <Box width="100%" height="fit-content" display="flex" flexDirection="column" sx={{ margin: '0 40px 40px 40px', paddingBottom: '60px', display: 'inline-block' }}>
-        <Box display='flex' width='100%' flexDirection='column' alignItems='center' >
-          <span style={{ fontSize: '28px', margin: '20px 0 0px 0' }}>Error 404</span><br/>
+        <Box display='flex' width='100%' flexDirection='column' alignItems='center'>
+          <span style={{ fontSize: '28px', margin: '20px 0 0px 0' }}>Error 404</span><br/> 
           <NoData redirect="true" text="The page you were looking for doesn't exists." />  
         </Box>
         </Box>
